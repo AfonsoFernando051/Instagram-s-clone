518b20d2649ba3dbc2424dc33de0c6bdafd58dd9
Fernando Afonso <afonsofernando051@gmail.com>
2022-05-11 16:09:28 -0300

    main

--- src/App.js
+++ src/App.js
@@ -1,34 +1,106 @@
 import React, { useState, useEffect } from "react";
 import "./App.css"
 import Post from "./Post";
-import Bleed from "./images/toronto1992.jpg"
-import Setup from "./images/setup.jpg"
-import Foto from "./images/foto.jpg"
-import db from "./firebase";
+import { auth, db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { Modal, makeStyles, Button, Input } from "@material-ui/core";
+import { classes } from "@material-ui/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "afonsofernando051",
-      caption: "Dia de aprendizado e estudo em ReactJS!",
-      imageUrl: Setup
-    },
-    {
-      username: "AliceInChains",
-      caption: "Incredible live in Toronto, 1992",
-      imageUrl: Bleed
-    },
-  ]);
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle)
 
-  // useEffect(() => {
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-  //   db.collection("posts").onSnapshot(snapshot => {
-  //     setPosts(snapshot.docs.map(doc => doc.data()))
-  //   })
-  // }, []);
+  async function loadPosts() {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    console.log('querySnapshot', querySnapshot)
+
+    const arr = [];
+    querySnapshot.forEach((doc) => {
+      arr.push(doc.data());
+    });
+    setPosts(arr);
+  }
+
+  useEffect(() => {
+    loadPosts()
+  }, []);
+
+  const signUp = (Event) => {
+
+    Event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+  }
 
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app_headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt="Instagram's logo"
+              />
+            </center>
+            <Input
+              placeholder="text"
+              type="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app_header">
         <img className="app_headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
@@ -36,14 +108,14 @@ function App() {
         />
       </div>
 
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+
       {
         posts.map(post => (
           <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
-      <Post username="afonsofernando051" caption="Dia de aprendizado e estudo em ReactJS!" imageUrl={Setup} />
-      <Post username="AliceInChains" caption="Incredible live in Toronto, 1992" imageUrl={Bleed} />
-      <Post username="afonsofernando051" caption="Incrível praia em Portugal!" imageUrl={Foto} />
+
     </div>
   )
 }
